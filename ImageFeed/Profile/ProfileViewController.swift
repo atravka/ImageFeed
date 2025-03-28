f179be4802224ba5f7a34a898a3059434fbcfc82
Andrey Travka <atravka@gmail.com>
2025-03-28 23:55:09 +0300

    Initial profile view

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Travka Andrey on 28.03.2025.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+
+    @IBOutlet private weak var userpickImage: UIImageView!
+    
+    @IBOutlet private weak var userNameLabel: UILabel!
+    
+    @IBOutlet private weak var loginNameLabel: UILabel!
+    
+    @IBOutlet private weak var descriptionLabel: UILabel!
+    
+    @IBOutlet private weak var exitButton: UIButton!
+    
+    @IBAction private func didTapExitButton() {
+    }
+    
+    
+}
