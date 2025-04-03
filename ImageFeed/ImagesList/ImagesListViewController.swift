b2059165904c78c9d82ad1019d3254eff4586b91
Andrey Travka <atravka@gmail.com>
2025-04-03 23:53:55 +0300

    SingleImageViewController 9/24 - 3/6 - 3/3

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -13,7 +13,8 @@ final class ImagesListViewController: UIViewController {
 
     private let showSingleImageSegueIdentifier = "ShowSingleImage"
     
-    private let photosName: [String] = Array(0..<20).map{ "\($0)" }
+    // private
+    let photosName: [String] = Array(0..<20).map{ "\($0)" }
     
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
@@ -43,8 +44,6 @@ final class ImagesListViewController: UIViewController {
             }
             
             let image = UIImage(named: photosName[indexPath.row])
-         //   _ = viewController.view
-         //   viewController.imageView.image = image
             viewController.image = image
         } else {
             super.prepare(for: segue, sender: sender)
